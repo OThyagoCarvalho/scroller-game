79ac8750a602e9b86173d6ba3efc77f773c542ae
Thyago Carvalho <othyagocarvalho@outlook.com>
2022-04-29 23:43:48 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import "./styles.css";
+
+const canvas = document.querySelector("canvas");
+console.log(canvas);
+
+const c = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Player {
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100
+    };
+    this.width = 30;
+    this.height = 30;
+  }
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
+
+const player = new Player();
+player.draw();
