0d1eeb0c2ed9d1a33fc7e40fc95d1dd5bc297688
Thyago Carvalho <othyagocarvalho@outlook.com>
2022-04-30 00:40:38 -0300

    add custom gravity

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import "./styles.css";
-
 const canvas = document.querySelector("canvas");
 console.log(canvas);
 
@@ -7,20 +5,45 @@ const c = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+const gravity = 0.4
 class Player {
   constructor() {
     this.position = {
       x: 100,
       y: 100
     };
+    this.velocity = {
+      x: 0,
+      y: 0,
+    }
     this.width = 30;
     this.height = 30;
   }
+
   draw() {
     c.fillStyle = "red";
     c.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
+
+  update(){
+    this.draw();
+    this.position.y += this.velocity.y 
+    if (this.position.y + this.height + this.velocity.y <= canvas.height){
+      this.velocity.y += gravity
+    } else {
+      this.velocity.y = 0;
+    }
+    
+  }
 }
 
 const player = new Player();
-player.draw();
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0,0, canvas.width,canvas.height)
+  player.update();
+  
+}
+
+animate();
